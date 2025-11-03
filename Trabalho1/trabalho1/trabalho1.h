4a7eaea4a01a3a4cdfd5fc213688c7a8d2e361a5
Maria Clara <mclaracosta30@gmail.com>
2025-11-02 22:01:47 -0300

    Update trabalho1.h

--- Trabalho1/trabalho1/trabalho1.h
+++ Trabalho1/trabalho1/trabalho1.h
@@ -51,4 +51,4 @@ int q6(int numerobase, int numerobusca);
 int q7(char matriz[8][10], char palavra[5]);
 DataQuebrada quebraData(char data[]);
 
-#endif  // MAIN_H
+#endif  // MARIACLARACOSTASANTOS-20251160006-T1
